b3252a447aba31cde00f8989c9b2437054a59592
SaminaLoha <lohawalasamina@gmail.com>
2020-08-02 16:57:43 -0400

    Updated comments

--- inputData.py
+++ inputData.py
@@ -1,4 +1,6 @@
 # This file contains input values to smallest_number.py
+# Start and end range keys are the values between which random numbers would be generated
+# nth_index is the index for nth smallest number - For eg. for below data the 7th smallest number would be the output
 
 
 class InputData:
